1a16021666c7ff2969a99ec0c4bd282382c19633
dc480506 <dev.c@somaiya.edu>
2019-08-24 23:41:24 +0530

    theme-change

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
-var m="text/x-java";
-const p=require('request');
+var editor = CodeMirror.fromTextArea(document.getElementById("demotext"), {
+          lineNumbers: true,
+          mode:"text/x-java",
+          theme:"darcula",
+          matchBrackets:true,
+          autoCloseBrackets:true,
+          autoCloseTags:true
+  });
+  editor.on('keyup', function(editor,event){
+      if( !(event.ctrlKey) && 
+        (event.keyCode>=65 && event.keyCode<=90)
+        ||(event.keyCode>=97 && event.keyCode<=122)
+        ||(event.keyCode>=46 && event.keyCode<=57)){
+       editor.showHint({completeSingle:false});
+    }
+  });
+document.getElementById("theme").addEventListener("click",function(){
+  if(editor.options.theme=="darcula"){
+     editor.setOption("theme","3024-day");
+    }else{
+      editor.setOption("theme","darcula");
+    }
+})
