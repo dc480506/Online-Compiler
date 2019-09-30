7b781f600b0abfaa9e2c595faaecfed016de7b25
dc480506 <dev.c@somaiya.edu>
2019-09-30 23:45:11 +0530

    Rename and Delete added

--- include/open_code.php
+++ include/open_code.php
@@ -1,10 +1,34 @@
 <?php
 session_start();
-include_once "config.php";
+include "config.php";
 $_SESSION['dir']=$base_dir.$_SESSION['u_user']."/".$_POST['code-name'];
 $_SESSION['file']="";
 $_SESSION['language']=$_POST['code-lang'];
 $_SESSION['code']=$_POST['code-name'];
+
+$user=$_SESSION['u_user'];
+$code=$_SESSION['code'];
+$dir=$_SESSION['dir'];
+
+if(isset($_POST['del_btn'])){
+    $sql="DELETE FROM code_info WHERE username='$user' AND codename='$code';";
+    mysqli_query($conn,$sql);
+    array_map('unlink', glob("$dir/*.*"));
+    rmdir($dir);
+    header("Location: ../html/add.php?code_deleted");
+    exit();
+}
+else if(isset($_POST['rename_btn'])){
+    $u1=mysqli_real_escape_string($conn,$_POST['rename']);
+    $sql="UPDATE code_info SET codename='$u1' WHERE username='$user' AND codename='$code';";
+    mysqli_query($conn,$sql);
+    $new=$base_dir.$_SESSION['u_user']."/".$u1;
+    rename($dir,$new);
+    header("Location: ../html/add.php?renamed");
+    exit();
+}
+
+
 if($_POST['code-lang']=="C"){
     $_SESSION['file']="main.c";
 }else if($_POST['code-lang']=="C++"){
@@ -16,4 +40,7 @@ if($_POST['code-lang']=="C"){
 }
 header("Location: ../html/editor.php");
 exit();
+
+
+
 ?>
--- js/add.js
+++ js/add.js
@@ -1,10 +1,30 @@
-document.getElementById('add-button').addEventListener('click',
- function(){
-	document.querySelector('.create-box').style.display='flex';
-	document.querySelector('.bg').style.opacity='0.4';
-});
-document.getElementById('cb-close').addEventListener('click',
-	function(){
-	document.querySelector('.create-box').style.display='none';
-	document.querySelector('.bg').style.opacity='1';
-   });
+document.querySelector('#add-button').addEventListener('click',
+    function() {
+        document.querySelector('.create-box').style.display = 'flex';
+        document.querySelector('.bg').style.opacity = '0.4';
+    });
+document.querySelector('.fa-window-close').addEventListener('click',
+    function() {
+        document.querySelector('.create-box').style.display = 'none';
+        document.querySelector('.bg').style.opacity = '1';
+    });
+
+var codename=document.getElementById("code-name");// input tag inside renamebox
+var codelang=document.getElementById("code-lang");// input tag inside renamebox
+var rename_btn=document.querySelectorAll('.r-btn');// rename button inside form tag
+var showRename=function() {
+    document.querySelector('.rename-box').style.display = 'flex';
+    document.querySelector('.bg').style.opacity = '0.4';
+    var a=this.getAttribute("name");// getting codename stored in button attribute name
+    var b=this.getAttribute("value");// getting language stored in button attribute value
+    codename.setAttribute("value",a);
+    codelang.setAttribute("value",b);
+}
+for(var i=0;i< rename_btn.length;i++){
+    rename_btn[i].addEventListener('click',showRename,false);
+}
+
+document.getElementById("close-rename").addEventListener("click",function(){
+   document.querySelector('.rename-box').style.display='none';
+   document.querySelector('.bg').style.opacity='1';
+},false);
