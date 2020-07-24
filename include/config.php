49c271ae39bf7d495a832e11463ebd8e4d232efd
dc480506 <dev.c@somaiya.edu>
2020-07-24 19:02:01 +0530

    Readme update

--- include/config.php
+++ include/config.php
@@ -4,9 +4,9 @@ $uname="root";
 $pwd="";
 $dbname="online-compiler";
 $conn = mysqli_connect($host, $uname, $pwd,$dbname);
-//$base_dir="/home/devu/UserCodes/";
+// $base_dir="/home/devu/UserCodes/";
 $base_dir="/var/UserCodes/";
-$CLIENT_ID="444425785443-5mh44gn88jrf46t217t7i4m62r4ui1ro.apps.googleusercontent.com";
+$CLIENT_ID="784580307034-0ta70nlq55f8q67tb9k4q31noar2n3r1.apps.googleusercontent.com";
 $google_api_path="/var/google-api-php-client-2.4.0/";
 //$rundir="/var/runfolder/"     ...Original
 //Added 
